d70b23f953939d87445f0963a73e2ad68aa6e627
Documenter.jl <documenter@juliadocs.github.io>
2023-12-27 14:42:42 +0000

    build based on 0271489

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FactorPlots","category":"page"},{"location":"#FactorPlots","page":"Home","title":"FactorPlots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FactorPlots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FactorPlots]","category":"page"}]
+}
